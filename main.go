38b9ecec75a19926a410e26770703b7f9989f49b
Allo <yin199909@aliyun.com>
2025-05-05 15:25:51 +0800

    parse flags

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"auto-cert/pkg/cdn"
 	"auto-cert/pkg/cert"
 	"auto-cert/pkg/dns"
+	"auto-cert/pkg/env"
 	"auto-cert/pkg/ref"
 	"auto-cert/pkg/tld"
 	"context"
@@ -22,70 +23,115 @@ const (
 	RenewBefore = 10 * 24 * time.Hour
 )
 
+type config struct {
+	email       string
+	fullDomain  string
+	tldPlusOne  string
+	subDomain   string
+	accountPath string
+
+	accessKey    string
+	accessSecret string
+}
+
 func main() {
 	var (
 		forceRenew        = false
 		createAcmeAccount = false
+		mustLoadAccount   bool
 	)
+	cfg := &config{}
 	flag.BoolVar(&forceRenew, "f", false, "force renew")
 	flag.BoolVar(&createAcmeAccount, "a", false, "create acme account")
+	flag.StringVar(&cfg.email, "e", env.GetString("EMAIL", ""), "email for acme account")
+	flag.StringVar(&cfg.fullDomain, "fd", env.GetString("FULL_DOMAIN", ""), "the full domain to generate certificate")
+	flag.StringVar(&cfg.tldPlusOne, "tp", env.GetString("TLD_PLUS_ONE", ""), "tld plus one")
+	flag.StringVar(&cfg.accountPath, "ap", env.GetString("ACCOUNT_FILE", "./account.json"), "account file path")
+	flag.BoolVar(&mustLoadAccount, "m", env.GetBool("MUST_LOAD_ACCOUNT", false), "must load account")
+	flag.StringVar(&cfg.accessKey, "ak", env.GetString("ACCESS_KEY", ""), "access key")
+	flag.StringVar(&cfg.accessSecret, "sk", env.GetString("ACCESS_SECRET", ""), "access secret")
 
-	fullDomain := mustLoadEnv("FULL_DOMAIN")
-	accountPath := loadEnvOr("ACCOUNT_FILE", "./account.json")
-	tldPlusOne := loadEnvOr("DOMAIN", "")
-	mustLoadAccount := loadEnvOr("MUST_LOAD_ACCOUNT", "false") == "true"
-
+	if cfg.email == "" {
+		log.Fatal("email is empty")
+	}
 	if createAcmeAccount {
 		// check if account exists
 		log.Println("creating acme account")
 		acmeCli := cert.NewClient()
-		err := createAndStoreAcmeAccount(context.Background(), acmeCli, accountPath)
+		err := createAndStoreAcmeAccount(context.Background(), acmeCli, cfg)
 		if err != nil {
 			log.Fatal(err)
 		}
 		return
 	}
 
-	subDomain := ""
-	if tldPlusOne == "" {
-		tld, err := tld.ParseDomain(fullDomain)
-		if err != nil {
-			log.Fatalf("parse domain failed: %v", err)
-		}
-		tldPlusOne = tld.TLDPlusOne
-		subDomain = tld.SubDomain
-	} else {
-		if !strings.HasSuffix(fullDomain, tldPlusOne) {
-			log.Fatalf("full domain %q is not match with domain %q\n", fullDomain, tldPlusOne)
-		}
-		subDomain = fullDomain[:len(fullDomain)-len(tldPlusOne)-1]
-		// now, the subDomain should be end with a dot or empty
-		if subDomain != "" && !strings.HasSuffix(subDomain, ".") {
-			log.Fatalf("full domain %q is not match with domain %q\n", fullDomain, tldPlusOne)
-		}
-		subDomain = strings.TrimSuffix(subDomain, ".")
+	if cfg.fullDomain == "" {
+		log.Fatal("full domain is empty")
 	}
+	cfg.subDomain = parseSubDomain(cfg)
 
-	ak := mustLoadEnv("ACCESS_KEY")
-	secret := mustLoadEnv("ACCESS_SECRET")
-	cfg := &openapi.Config{
-		AccessKeyId:     ref.GetPointer(ak),
-		AccessKeySecret: ref.GetPointer(secret),
+	if cfg.accessKey == "" || cfg.accessSecret == "" {
+		log.Fatal("access key or access secret is empty")
 	}
-	cdnCli, err := cdn.CreateClient(cfg)
+	apiCfg := &openapi.Config{
+		AccessKeyId:     ref.GetPointer(cfg.accessKey),
+		AccessKeySecret: ref.GetPointer(cfg.accessSecret),
+	}
+	cdnCli, err := cdn.CreateClient(apiCfg)
 	if err != nil {
 		log.Fatalf("create cdn client failed: %v", err)
 	}
 
 	// check domain's certificate
-	certInfo, err := cdnCli.QueryDomainCertificate(fullDomain)
+	if !shouldRenewCert(cdnCli, forceRenew, cfg) {
+		return
+	}
+
+	dnsCli, err := dns.CreateClient(apiCfg)
+	if err != nil {
+		log.Fatalf("create dns client failed: %v", err)
+	}
+
+	acmeCli := cert.NewClient()
+	ctx := context.Background()
+	err = acmeCli.LoadAccount(ctx, cfg.accountPath)
+	if err != nil {
+		if mustLoadAccount {
+			log.Fatalf("load account failed: %v", err)
+		}
+		log.Printf("account not found (%s), register it", err.Error())
+		err = createAndStoreAcmeAccount(ctx, acmeCli, cfg)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	c, err := acmeCli.GenerateCert(ctx, cfg.fullDomain, cert.KeyECDSA, resolveDnsChallengeFunc(dnsCli, cfg))
+	if err != nil {
+		log.Fatalf("generate certificate failed: %v", err)
+	}
+
+	// upload certificate
+	err = cdnCli.SetCert(cfg.fullDomain, c)
+	if err != nil {
+		log.Fatalf("set certificate failed: %v", err)
+	}
+	log.Printf("certificate set")
+}
+
+func shouldRenewCert(cdnCli *cdn.Client, forceRenew bool, cfg *config) bool {
+	if forceRenew {
+		log.Print("force renew the certificate")
+		return true
+	}
+	certInfo, err := cdnCli.QueryDomainCertificate(cfg.fullDomain)
 	if err != nil {
 		log.Fatalf("query domain certificate failed: %v", err)
 	}
 	duration := time.Until(certInfo.ExpireTime)
 	if duration > RenewBefore {
 		log.Printf("certificate is not expired, skip renew")
-		return
+		return false
 	}
 
 	if duration <= 0 {
@@ -93,29 +139,48 @@ func main() {
 	} else {
 		log.Printf("certificate will be expired in %v, renew it", duration)
 	}
+	return true
+}
 
-	dnsCli, err := dns.CreateClient(cfg)
-	if err != nil {
-		log.Fatalf("create dns client failed: %v", err)
+func parseSubDomain(cfg *config) string {
+	if cfg.tldPlusOne == "" {
+		tld, err := tld.ParseDomain(cfg.fullDomain)
+		if err != nil {
+			log.Fatalf("parse domain failed: %v", err)
+		}
+		cfg.tldPlusOne = tld.TLDPlusOne
+		return tld.SubDomain
 	}
 
-	resolveDnsChallenge := func(ctx context.Context, ch *acme.Challenge, getToken cert.GetChallengeToken, accept cert.AcceptChallenge) error {
+	if !strings.HasSuffix(cfg.fullDomain, cfg.tldPlusOne) {
+		log.Fatalf("full domain %q is not match with domain %q\n", cfg.fullDomain, cfg.tldPlusOne)
+	}
+	subDomain := cfg.fullDomain[:len(cfg.fullDomain)-len(cfg.tldPlusOne)-1]
+	// now, the subDomain should be end with a dot or empty
+	if subDomain != "" && !strings.HasSuffix(subDomain, ".") {
+		log.Fatalf("full domain %q is not match with domain %q\n", cfg.fullDomain, cfg.tldPlusOne)
+	}
+	return strings.TrimSuffix(subDomain, ".")
+}
+
+func resolveDnsChallengeFunc(dnsCli *dns.Client, cfg *config) func(ctx context.Context, ch *acme.Challenge, getToken cert.GetChallengeToken, accept cert.AcceptChallenge) error {
+	return func(ctx context.Context, ch *acme.Challenge, getToken cert.GetChallengeToken, accept cert.AcceptChallenge) error {
 		if ch.Type != cert.AcmeChallengeDNS01 {
 			return cert.ErrNotImplemented
 		}
 
-		rr := cert.GetDNS01ChallengeRecord(subDomain)
+		rr := cert.GetDNS01ChallengeRecord(cfg.subDomain)
 		value, err := getToken(ch.Token)
 		if err != nil {
 			return err
 		}
 
-		err = dnsCli.AddOrSetTextRecord(tldPlusOne, rr, value)
+		err = dnsCli.AddOrSetTextRecord(cfg.tldPlusOne, rr, value)
 		if err != nil {
 			return err
 		}
 
-		defer dnsCli.DeleteTextRecord(tldPlusOne, rr)
+		defer dnsCli.DeleteTextRecord(cfg.tldPlusOne, rr)
 
 		// sleep for a while to make sure the record is effective
 		timer := time.NewTimer(5 * time.Second)
@@ -128,36 +193,10 @@ func main() {
 
 		return accept(ctx)
 	}
-
-	acmeCli := cert.NewClient()
-	ctx := context.Background()
-	err = acmeCli.LoadAccount(ctx, accountPath)
-	if err != nil {
-		if mustLoadAccount {
-			log.Fatalf("load account failed: %v", err)
-		}
-		log.Printf("account not found (%s), register it", err.Error())
-		err = createAndStoreAcmeAccount(ctx, acmeCli, accountPath)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	c, err := acmeCli.GenerateCert(ctx, fullDomain, cert.KeyECDSA, resolveDnsChallenge)
-	if err != nil {
-		log.Fatalf("generate certificate failed: %v", err)
-	}
-
-	// upload certificate
-	err = cdnCli.SetCert(fullDomain, c)
-	if err != nil {
-		log.Fatalf("set certificate failed: %v", err)
-	}
-	log.Printf("certificate set")
 }
 
-func createAndStoreAcmeAccount(ctx context.Context, acmeCli *cert.Client, accountPath string) error {
-	err := acmeCli.RegisterAccount(ctx, mustLoadEnv("EMAIL"))
+func createAndStoreAcmeAccount(ctx context.Context, acmeCli *cert.Client, cfg *config) error {
+	err := acmeCli.RegisterAccount(ctx, cfg.email)
 	if err != nil {
 		return fmt.Errorf("register account failed: %w", err)
 	}
@@ -167,27 +206,11 @@ func createAndStoreAcmeAccount(ctx context.Context, acmeCli *cert.Client, accoun
 	if err != nil {
 		return fmt.Errorf("export account failed: %w", err)
 	}
-	err = os.WriteFile(accountPath, data, 0600)
+	err = os.WriteFile(cfg.accountPath, data, 0600)
 	if err != nil {
 		log.Printf("failed to store account, please store it manually, account content:\n%s\n", string(data))
 		return fmt.Errorf("store account failed: %w", err)
 	}
-	log.Printf("account stored at %s\n", accountPath)
+	log.Printf("account stored at %s\n", cfg.accountPath)
 	return nil
-}
-
-func mustLoadEnv(key string) string {
-	v := os.Getenv(key)
-	if v == "" {
-		log.Fatalf("env %s is empty", key)
-	}
-	return v
-}
-
-func loadEnvOr(key, def string) string {
-	v := os.Getenv(key)
-	if v == "" {
-		return def
-	}
-	return v
 }
